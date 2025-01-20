d3519707bd87e5db1825c691584ce133142dc96e
mdsaj <mdsajjadhossain614@gmail.com>
2025-01-20 14:22:17 +0600

    search field

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Home.css";
 import home_truck from './image/home_truck.jpg'
+import SearchInput from "./SearchInput";
 
 const Home = () => {
     return (
         <div className="landing-container">
+            {/*Hero section*/}
             <div className="text-container">
                 <h1 className="slide-from-left">Truck Lagbo ?</h1>
                 <h5 className="slide-from-left">Search truck in your area...</h5>
@@ -16,6 +18,13 @@ const Home = () => {
                     className="slide-from-right"
                 />
             </div>
+
+        {/*    Search field*/}
+            <div align="center" className="search-container">
+                <SearchInput/>
+            </div>
+
+
         </div>
     );
 };
