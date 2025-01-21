c4339bdc5881847ecc75f541fb91902a959c880f
mdsaj <mdsajjadhossain614@gmail.com>
2025-01-21 18:28:21 +0600

    profile avatar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Header.css';
 import {Button} from "reactstrap";
-import {Avatar} from "@mui/material";
+import {Avatar, IconButton, Menu, MenuItem} from "@mui/material";
 
 export default function Header() {
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    // Handle menu open
+    const handleMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    // Handle menu close
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    // Menu item click handler
+    const handleMenuItemClick = (option) => {
+        console.log(`${option} clicked`);
+        handleMenuClose();
+    };
+
     return (<div className="header-container">
         <div className="header-logo">
             <span>Truck Lagbo ?</span>
@@ -20,7 +40,35 @@ export default function Header() {
                 </Button>
             </div>
             <div>
-                <Avatar alt="Sajjad Hossain" src="/static/images/avatar/1.jpg"/>
+                    <Avatar onClick={handleMenuOpen} alt="Sajjad Hossain" src="/static/images/avatar/1.jpg"/>
+
+                {/* Dropdown Menu */}
+                <Menu
+                    anchorEl={anchorEl} // Menu position reference
+                    open={open} // Is the menu open
+                    onClose={handleMenuClose} // Close menu on outside click
+                    anchorOrigin={{
+                        vertical: "bottom",
+                        horizontal: "right",
+                    }}
+                    transformOrigin={{
+                        vertical: "top",
+                        horizontal: "right",
+                    }}
+                >
+                    {/* Menu Options */}
+                    <MenuItem onClick={() => handleMenuItemClick("Profile")}>
+                        Profile
+                    </MenuItem>
+                    <MenuItem onClick={() => handleMenuItemClick("Settings")}>
+                        Settings
+                    </MenuItem>
+                    <MenuItem onClick={() => handleMenuItemClick("Logout")}>
+                        Logout
+                    </MenuItem>
+                    <MenuItem onClick={() => handleMenuItemClick("Login")}>Login</MenuItem>
+                </Menu>
+
             </div>
         </div>
     </div>);
